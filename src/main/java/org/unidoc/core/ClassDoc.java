7acfcf11003f1bea8996b1a70f29512d99897237
charles <dinneyacharles007@gmail.com>
2020-07-14 23:05:43 +0100

    add properties to ClassDoc annotation and add test for Unidoc

--- src/main/java/org/unidoc/core/ClassDoc.java
+++ src/main/java/org/unidoc/core/ClassDoc.java
@@ -1,8 +1,16 @@
 package org.unidoc.core;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(value = RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface ClassDoc {
+
+    String author() default "";
+
+    String description() default "";
+
 }
---
+++ src/test/java/org/unidocTest/UnidocTest.java
@@ -0,0 +1,7 @@
+package org.unidocTest;
+
+import org.unidoc.core.ClassDoc;
+
+@ClassDoc(author = "charles", description = "cgadsf")
+public class UnidocTest {
+}
